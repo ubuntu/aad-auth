36440aadd00939de10a21043d56b1b7b0b30d4f4
Didier Roche <didier.roche@canonical.com>
2022-07-19 10:38:41 +0200

    Fix typo
    Co-authored-by: Gabriel Nagy <gabriel.nagy@canonical.com>

--- internal/user/user.go
+++ internal/user/user.go
@@ -2,7 +2,7 @@ package user
 
 import "strings"
 
-// NormalizeName returned a normalized, lowercase version of the username as
+// NormalizeName returns a normalized, lowercase version of the username as
 // AnYCaSe@DomAIN is accepted by aad.
 func NormalizeName(name string) string {
 	return strings.ToLower(name)
