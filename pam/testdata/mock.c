7bbc3928ad9a51c288caa5ca89912612f7fe7833
Didier Roche <didier.roche@canonical.com>
2022-08-04 16:27:10 +0200

    Add PAM integration tests
    We control our mock when building the library with the itnegrationtests
    build tags, then link our PAM local instance against it, provide our own
    configuration.
    Logs are always debug logs on stderr.

---
+++ pam/testdata/mock.c
@@ -0,0 +1,54 @@
+/*
+ This program was contributed by Shane Watts
+ [modifications by AGM and kukuk]
+ You need to add the following (or equivalent) to the
+ /etc/pam.d/check_user file:
+ # check authorization
+ auth required pam_unix.so
+ account required pam_unix.so
+ */
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
+#include <stdio.h>
+static struct pam_conv conv = {
+    misc_conv,
+    NULL
+};
+
+int main(int argc, char *argv[])
+{
+    pam_handle_t *pamh=NULL;
+    int retval;
+    const char *user="nobody";
+
+    if(argc == 2) {
+        user = argv[1];
+    }
+
+    if(argc > 2) {
+        fprintf(stderr, "Usage: check_user [username]\n");
+        exit(1);
+    }
+
+    retval = pam_start_confdir("aadtest", user, &conv, ".", &pamh);
+    if (retval == PAM_SUCCESS)
+    retval = pam_authenticate(pamh, 0); /* is user really user? */
+    //if (retval == PAM_SUCCESS)
+    //    retval = pam_acct_mgmt(pamh, 0); /* permitted access? */
+
+    /* This is where we have been authorized or not. */
+    if (retval == PAM_SUCCESS) {
+        fprintf(stdout, "Authenticated\n");
+    } else {
+        fprintf(stdout, "Not Authenticated\n");
+    }
+
+    if (pam_end(pamh,retval) != PAM_SUCCESS) { /* close Linux-PAM */
+        pamh = NULL;
+        fprintf(stderr, "check_user: failed to release authenticator\n");
+        exit(1);
+    }
+
+    return ( retval == PAM_SUCCESS ? 0:1 ); /* indicate success */
+}
+
