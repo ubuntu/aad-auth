d025a429a3dff0e5d45b926946b93f0667c06a46
Didier Roche <didier.roche@canonical.com>
2022-08-08 13:47:00 +0200

    nss passwd integration tests
    Add ByName and ByUID integration tests when calling getent.

--- nss/passwd_test.go
+++ nss/passwd_test.go
@@ -10,6 +10,124 @@ import (
 )
 
 // TODO: process coverage once https://github.com/golang/go/issues/51430 is implemented in Go.
+func TestNssGetPasswdByName(t *testing.T) {
+	t.Parallel()
+
+	uid, gid := testutils.GetCurrentUidGid(t)
+
+	tests := map[string]struct {
+		name string
+
+		cacheDB   string
+		rootUid   int
+		shadowGid int
+
+		wantErr bool
+	}{
+		"list existing user": {},
+		"access to shadow is not needed to list existing user": {shadowGid: 4242},
+
+		"no cache no error on existing local user": {name: "root", cacheDB: "-"},
+
+		// error cases
+		"user does not exists":                        {name: "doesnotexist@domain.com", wantErr: true},
+		"no cache can't get user":                     {cacheDB: "-", wantErr: true},
+		"invalid permissions on cache can't get user": {rootUid: 4242, wantErr: true},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cacheDir := t.TempDir()
+			if tc.name == "" {
+				tc.name = "myuser@domain.com"
+			}
+			if tc.cacheDB == "" {
+				tc.cacheDB = "users_in_db"
+			}
+			if tc.cacheDB != "-" {
+				testutils.CopyDBAndFixPermissions(t, filepath.Join("testdata", tc.cacheDB), cacheDir)
+			}
+
+			if tc.rootUid == 0 {
+				tc.rootUid = uid
+			}
+			if tc.shadowGid == 0 {
+				tc.shadowGid = gid
+			}
+
+			got, err := outNSSCommandForLib(t, tc.rootUid, gid, tc.shadowGid, cacheDir, nil, "getent", "passwd", tc.name)
+			if tc.wantErr {
+				require.Error(t, err, "getent should have errored out but didn't")
+				return
+			}
+			require.NoError(t, err, "getent should succeed")
+
+			want := testutils.SaveAndLoadFromGolden(t, got)
+			require.Equal(t, want, got, "Should get expected aad user")
+		})
+	}
+}
+func TestNssGetPasswdByUID(t *testing.T) {
+	t.Parallel()
+
+	uid, gid := testutils.GetCurrentUidGid(t)
+
+	tests := map[string]struct {
+		uid string
+
+		cacheDB   string
+		rootUid   int
+		shadowGid int
+
+		wantErr bool
+	}{
+		"list existing user": {},
+		"access to shadow is not needed to list existing user": {shadowGid: 4242},
+
+		"no cache no error on existing local user": {uid: "0", cacheDB: "-"},
+
+		// error cases
+		"user does not exists":                        {uid: "4242", wantErr: true},
+		"no cache can't get user":                     {cacheDB: "-", wantErr: true},
+		"invalid permissions on cache can't get user": {rootUid: 4242, wantErr: true},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cacheDir := t.TempDir()
+			if tc.uid == "" {
+				tc.uid = "1929326240"
+			}
+			if tc.cacheDB == "" {
+				tc.cacheDB = "users_in_db"
+			}
+			if tc.cacheDB != "-" {
+				testutils.CopyDBAndFixPermissions(t, filepath.Join("testdata", tc.cacheDB), cacheDir)
+			}
+
+			if tc.rootUid == 0 {
+				tc.rootUid = uid
+			}
+			if tc.shadowGid == 0 {
+				tc.shadowGid = gid
+			}
+
+			got, err := outNSSCommandForLib(t, tc.rootUid, gid, tc.shadowGid, cacheDir, nil, "getent", "passwd", tc.uid)
+			if tc.wantErr {
+				require.Error(t, err, "getent should have errored out but didn't")
+				return
+			}
+			require.NoError(t, err, "getent should succeed")
+
+			want := testutils.SaveAndLoadFromGolden(t, got)
+			require.Equal(t, want, got, "Should get expected aad user")
+		})
+	}
+}
 func TestNssGetPasswd(t *testing.T) {
 	t.Parallel()
 
