5073faa1fd1e00c8bd1ca1ac909a5d399469cf74
denisonbarbosa <denison.barbosa@canonical.com>
2022-08-25 11:22:13 -0400

    Removing obsolete functions

--- internal/cache/helper_test.go
+++ internal/cache/helper_test.go
@@ -1,14 +1,5 @@
 package cache_test
 
-import (
-	"context"
-	"sort"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-	"github.com/ubuntu/aad-auth/internal/cache"
-)
-
 type userInfos struct {
 	name     string
 	uid      int64
@@ -29,33 +20,4 @@ func init() {
 	for _, info := range usersForTests {
 		usersForTestsByUID[uint(info.uid)] = info
 	}
-}
-
-// insertUsersInDb inserts usersForTests after opening a cache at cacheDir.
-func insertUsersInDb(t *testing.T, cacheDir string) {
-	t.Helper()
-
-	c := cache.NewCacheForTests(t, cacheDir, cache.WithTeardownDuration(0))
-	defer c.Close(context.Background())
-
-	// The randomness in map iterating was causing problems with the tests.
-	// Some test users were getting different IDs based on the order they were
-	// inserted in the cache. To fix that, the test users will be inserted in
-	// ASCII order.
-	keys := getSortedKeys(usersForTests)
-
-	for _, k := range keys {
-		u := usersForTests[k]
-		err := c.Update(context.Background(), u.name, u.password, "/home/%f", "/bin/bash")
-		require.NoError(t, err, "Setup: can't insert user %v to db", u.name)
-	}
-}
-
-func getSortedKeys(usersMap map[string]userInfos) []string {
-	keys := make([]string, 0, len(usersForTests))
-	for k := range usersMap {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
 }
