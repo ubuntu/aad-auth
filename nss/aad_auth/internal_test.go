db9c623f58c342b996977f7d7e001971db869a34
denisonbarbosa <denison.barbosa@canonical.com>
2022-09-01 12:08:54 -0400

    Improving the internal tests for fmtOutput
    Adding some more testcases and helpers in mock_test to increase our
    options when trying to test the formatter (might be overkill, but isn't
    hurting anyone).

--- nss/aad_auth/internal_test.go
+++ nss/aad_auth/internal_test.go
@@ -14,15 +14,18 @@ func TestFmtOutput(t *testing.T) {
 	t.Parallel()
 
 	tests := map[string]struct {
-		err error
+		nEntries int
+		err      error
 	}{
-		"properly format with error ErrTryAgainEAgain":    {err: nss.ErrTryAgainEAgain},
-		"properly format with error ErrTryAgainERange":    {err: nss.ErrTryAgainERange},
-		"properly format with error ErrUnavailableENoEnt": {err: nss.ErrUnavailableENoEnt},
-		"properly format with error ErrNotFoundENoEnt":    {err: nss.ErrNotFoundENoEnt},
-		"properly format with error ErrNotFoundSuccess":   {err: nss.ErrNotFoundSuccess},
-		"properly format with unknown Err":                {err: fmt.Errorf("SomeError")},
-		"properly format with nil error":                  {err: nil},
+		"properly format with error ErrTryAgainEAgain":    {err: nss.ErrTryAgainEAgain, nEntries: 1},
+		"properly format with error ErrTryAgainERange":    {err: nss.ErrTryAgainERange, nEntries: 1},
+		"properly format with error ErrUnavailableENoEnt": {err: nss.ErrUnavailableENoEnt, nEntries: 1},
+		"properly format with error ErrNotFoundENoEnt":    {err: nss.ErrNotFoundENoEnt, nEntries: 1},
+		"properly format with error ErrNotFoundSuccess":   {err: nss.ErrNotFoundSuccess, nEntries: 1},
+		"properly format with unknown Err":                {err: fmt.Errorf("SomeError"), nEntries: 1},
+		"properly format with nil error":                  {err: nil, nEntries: 1},
+		"properly format with multiple entries":           {err: nil, nEntries: 5},
+		"properly format with no entries":                 {err: nil, nEntries: 0},
 	}
 
 	for name, tc := range tests {
@@ -30,10 +33,7 @@ func TestFmtOutput(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			entries := []string{"myuser@domain.com:x:1929326240:1929326240::/home/myuser@domain.com:/bin/bash"}
-
-			got := fmtOutput(context.Background(), entries, tc.err)
-
+			got := fmtGetentOutput(context.Background(), getMockEntries(tc.nEntries), tc.err)
 			want := testutils.LoadAndUpdateFromGolden(t, got)
 			require.Equal(t, want, got, "Formatted output must match")
 		})
