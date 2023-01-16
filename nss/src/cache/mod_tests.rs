ea2960a52ef54a7fa162adf274131fc4715213f4
Didier Roche <didier.roche@canonical.com>
2023-01-16 15:45:44 +0100

    Detect family_test/subtest or bare family test cases
    Enhance the helpers to detect both cases and return meaningful golden
    files based on that.

--- nss/src/cache/mod_tests.rs
+++ nss/src/cache/mod_tests.rs
@@ -12,7 +12,6 @@ use crate::CacheDB;
 #[test_case(4242, true  ; "Error on non existing user")]
 fn test_get_passwd_from_uid(uid: u32, want_err: bool) {
     let module_path = testutils::get_module_path(file!());
-    let test_name = testutils::current_test_name();
 
     let cache_dir =
         TempDir::new("test-aad-auth").expect("Setup: could not create temporary cache directory");
@@ -43,7 +42,8 @@ fn test_get_passwd_from_uid(uid: u32, want_err: bool) {
     let got = to_string(&got.unwrap()).unwrap();
 
     let mut mint = testutils::golden_mint(&module_path);
-    let mut golden = mint.new_goldenfile(test_name.1).unwrap();
+    let (_, sub_test_name) = testutils::current_test_name();
+    let mut golden = mint.new_goldenfile(sub_test_name.unwrap()).unwrap();
     golden
         .write_all(got.as_bytes())
         .expect("Teardown: can't write to file to compare with golden");
--- nss/src/testutils/mod.rs
+++ nss/src/testutils/mod.rs
@@ -14,9 +14,10 @@ pub fn get_module_path(path: &str) -> String {
     path
 }
 
-// current_test_name returns a tuple of (parent_test_name, sub_test_name).
+// current_test_name returns a tuple of (family_name, sub_test_name).
+// Note that it can be called on tests without subtests too.
 // The detection is based on thread name, and so, does not work when RUST_TEST_THREADS=1.
-pub fn current_test_name() -> (String, String) {
+pub fn current_test_name() -> (String, Option<String>) {
     #[allow(clippy::or_fun_call)]
     if env::var("RUST_TEST_THREADS").unwrap_or("".to_string()) == "1" {
         panic!("Tests could not run with RUST_TEST_THREADS=1")
@@ -24,20 +25,31 @@ pub fn current_test_name() -> (String, String) {
 
     let cur_thread = std::thread::current();
     let parts: Vec<&str> = cur_thread.name().unwrap().split("::").collect();
-    (
-        parts[parts.len() - 2].to_string(),
-        parts[parts.len() - 1].to_string().to_lowercase(),
-    )
+    let family_name = parts[parts.len() - 2].to_string().to_lowercase();
+    let subtest_name = parts[parts.len() - 1].to_string().to_lowercase();
+    if subtest_name.starts_with("test_") {
+        return (subtest_name, None);
+    }
+    (family_name, Some(subtest_name))
 }
 
 // golden_mint returns the golden Mint based on the family test name of current test.
+// If there is no subtests, then, it returns the parent directory based on test name
+// where you will add a "golden"
+// file.
 // The detection is based on thread name, and so, does not work when RUST_TEST_THREADS=1.
 pub fn golden_mint(module_path: &str) -> Mint {
-    let (family_name, _) = current_test_name();
-    Mint::new(
-        Path::new(module_path)
-            .join("testdata")
-            .join(&family_name)
-            .join("golden"),
-    )
+    let (family_name, subtest_name) = current_test_name();
+
+    match subtest_name {
+        // If there is a subtest, "golden" is then the Mint director.
+        Some(_) => Mint::new(
+            Path::new(module_path)
+                .join("testdata")
+                .join(&family_name)
+                .join("golden"),
+        ),
+        // If there is none, then the directory name is the family name and golden is the file.
+        None => Mint::new(Path::new(module_path).join("testdata").join(&family_name)),
+    }
 }
