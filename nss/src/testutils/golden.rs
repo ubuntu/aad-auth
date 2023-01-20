afcda3d99573bdec704c5a141156c5907f355269
denisonbarbosa <denison.barbosa@canonical.com>
2023-01-20 07:50:08 -0400

    Applying standards in src/testutils

--- nss/src/testutils/golden.rs
+++ nss/src/testutils/golden.rs
@@ -2,11 +2,11 @@ use super::current_test_name;
 use goldenfile::Mint;
 use std::path::Path;
 
-// golden_mint returns the golden Mint based on the family test name of current test.
-// If there is no subtests, then, it returns the parent directory based on test name
-// where you will add a "golden"
-// file.
-// The detection is based on thread name, and so, does not work when RUST_TEST_THREADS=1.
+/// golden_mint returns the golden Mint based on the family test name of current test.
+/// If there is no subtests, then, it returns the parent directory based on test name
+/// where you will add a "golden" file.
+///
+/// The detection is based on thread name, and so, does not work when RUST_TEST_THREADS=1.
 pub fn golden_mint(module_path: &str) -> Mint {
     let (family_name, subtest_name) = current_test_name();
 
