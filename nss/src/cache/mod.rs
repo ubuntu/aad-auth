15de11f699996aedea9b56a700e48867e5e7fcd8
denisonbarbosa <denison.barbosa@canonical.com>
2023-01-20 07:49:18 -0400

    Applying standards in src/cache

--- nss/src/cache/mod.rs
+++ nss/src/cache/mod.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use rusqlite::{params, Connection, OpenFlags, Rows, Statement};
+use rusqlite::{Connection, OpenFlags, Rows, Statement};
 use serde::Serialize;
 
 use log::debug;
@@ -10,7 +10,7 @@ mod mod_tests;
 
 const DB_PATH: &str = "/var/lib/aad/cache";
 
-// Passwd struct represents a password entry in the cache database.
+/// Passwd struct represents a password entry in the cache database.
 #[derive(Debug, Serialize)]
 pub struct Passwd {
     pub name: String,
@@ -22,7 +22,7 @@ pub struct Passwd {
     pub shell: String,
 }
 
-// CacheError enum represents the list of errors supported by the cache.
+/// CacheError enum represents the list of errors supported by the cache.
 #[derive(Debug)]
 pub enum CacheError {
     DatabaseError(String),
@@ -30,28 +30,31 @@ pub enum CacheError {
     NoRecord,
 }
 
-// CacheDB struct represents the cache database.
+/// CacheDB struct represents the cache database.
 pub struct CacheDB {
     conn: Connection,
 }
 
-// CacheDBBuilder struct is the struct for the builder pattern and change the parameters of the cache.
+/// CacheDBBuilder struct is the struct for the builder pattern and change the parameters of the cache.
 pub struct CacheDBBuilder {
     db_path: Option<String>,
 }
 
 impl CacheDBBuilder {
-    // with_db_path overrides the path to the cache database.
+    /// with_db_path overrides the path to the cache database.
     pub fn with_db_path(&mut self, db_path: &str) -> &mut Self {
         debug!("using custom db path: {}", db_path);
         self.db_path = Some(db_path.to_string());
         self
     }
 
-    // build initialize and open a connection to the cache database.
+    /// build initializes and opens a connection to the cache database.
     pub fn build(&self) -> Result<CacheDB, CacheError> {
-        #[allow(clippy::or_fun_call)]
-        let db_path = self.db_path.clone().unwrap_or(DB_PATH.to_string());
+        let mut db_path = DB_PATH.to_string();
+        if let Some(path_override) = &self.db_path {
+            db_path = path_override.to_string();
+        }
+
         let passwd_db = Path::new(&db_path).join("passwd.db");
         let passwd_db = passwd_db.to_str().unwrap();
 
@@ -59,7 +62,7 @@ impl CacheDBBuilder {
 
         let conn = match Connection::open_with_flags(passwd_db, OpenFlags::SQLITE_OPEN_READ_ONLY) {
             Ok(conn) => conn,
-            Err(e) => return Err(CacheError::DatabaseError(e.to_string())),
+            Err(err) => return Err(CacheError::DatabaseError(err.to_string())),
         };
 
         // TODO: attach shadow if root. Handle file permissions…
@@ -69,74 +72,69 @@ impl CacheDBBuilder {
 }
 
 impl CacheDB {
-    // new creates a new CacheDBBuilder object.
+    /// new creates a new CacheDBBuilder object.
     #[allow(clippy::new_ret_no_self)] // builder pattern
     pub fn new() -> CacheDBBuilder {
         CacheDBBuilder { db_path: None }
     }
 
     /* Passwd */
-    // get_passwd_from_uid returns a password entry by user id.
-    pub fn get_passwd_from_uid(self: &CacheDB, uid: u32) -> Result<Passwd, CacheError> {
+    /// get_passwd_by_uid queries the database for a passwd row with matching uid.
+    pub fn get_passwd_by_uid(&self, uid: u32) -> Result<Passwd, CacheError> {
         let mut stmt = self.prepare_statement(
             "SELECT login, password, uid, gid, gecos, home, shell FROM passwd WHERE uid = ?", // Last empty field is the shadow password
         )?;
 
-        let rows = match stmt.query(params![uid]) {
+        let rows = match stmt.query([uid]) {
             Ok(rows) => rows,
-            Err(error) => return Err(CacheError::QueryError(error.to_string())),
+            Err(err) => return Err(CacheError::QueryError(err.to_string())),
         };
 
-        let mut entries = CacheDB::rows_to_passwd_entries(rows);
+        let mut entries = Self::rows_to_passwd_entries(rows);
 
-        let passwd = CacheDB::expect_one_row(&mut entries)?;
-        Ok(passwd)
+        Self::expect_one_row(&mut entries)
     }
 
-    // get_passwd_from_name returns a password entry by user name.
-    pub fn get_passwd_from_name(self: &CacheDB, login: &str) -> Result<Passwd, CacheError> {
+    /// get_passwd_by_name queries the database for a passwd row with matching name.
+    pub fn get_passwd_by_name(&self, login: &str) -> Result<Passwd, CacheError> {
         let mut stmt = self.prepare_statement(
             "SELECT login, password, uid, gid, gecos, home, shell FROM passwd WHERE login = ?", // Last empty field is the shadow password
         )?;
 
-        let rows = match stmt.query(params![login]) {
+        let rows = match stmt.query([login]) {
             Ok(rows) => rows,
-            Err(error) => return Err(CacheError::QueryError(error.to_string())),
+            Err(err) => return Err(CacheError::QueryError(err.to_string())),
         };
 
-        let mut entries = CacheDB::rows_to_passwd_entries(rows);
+        let mut entries = Self::rows_to_passwd_entries(rows);
 
-        let passwd = CacheDB::expect_one_row(&mut entries)?;
-        Ok(passwd)
+        Self::expect_one_row(&mut entries)
     }
 
-    // get_all_passwd returns all the password entries.
-    pub fn get_all_passwd(self: &CacheDB) -> Result<Vec<Passwd>, CacheError> {
+    /// get_all_passwd queries the database for all passwd rows.
+    pub fn get_all_passwd(&self) -> Result<Vec<Passwd>, CacheError> {
         let mut stmt = self.prepare_statement(
             "SELECT login, password, uid, gid, gecos, home, shell FROM passwd", // Last empty field is the shadow password
         )?;
 
-        let rows = match stmt.query(params![]) {
+        let rows = match stmt.query([]) {
             Ok(rows) => rows,
-            Err(error) => return Err(CacheError::QueryError(error.to_string())),
+            Err(err) => return Err(CacheError::QueryError(err.to_string())),
         };
 
-        let entries = CacheDB::rows_to_passwd_entries(rows);
-        Ok(entries)
+        Ok(Self::rows_to_passwd_entries(rows))
     }
 
     /* Common */
-    // prepare_statement prepares a query.
-    fn prepare_statement(self: &CacheDB, request: &str) -> Result<Statement, CacheError> {
-        let stmt = match self.conn.prepare(request) {
-            Ok(stmt) => stmt,
-            Err(e) => return Err(CacheError::QueryError(e.to_string())),
-        };
-
-        Ok(stmt)
+    /// prepare_statement prepares a statement and queries the database.
+    fn prepare_statement(&self, stmt_str: &str) -> Result<Statement, CacheError> {
+        match self.conn.prepare(stmt_str) {
+            Ok(stmt) => Ok(stmt),
+            Err(err) => Err(CacheError::QueryError(err.to_string())),
+        }
     }
 
-    // rows_to_passwd_entries converts SQL rows to a list of password entries.
+    /// rows_to_passwd_entries converts SQL rows to `Vec<Passwd>`.
     fn rows_to_passwd_entries(mut rows: Rows) -> Vec<Passwd> {
         let mut entries = Vec::new();
         while let Ok(Some(row)) = rows.next() {
@@ -153,23 +151,17 @@ impl CacheDB {
         entries
     }
 
-    // expect_one_row returns an error if the list of entries contains no rows or more than one row.
+    /// expect_one_row returns an error if `entries` contains no rows or more than one row.
     fn expect_one_row<T>(entries: &mut Vec<T>) -> Result<T, CacheError> {
-        if entries.is_empty() {
-            return Err(CacheError::NoRecord);
-        }
-
         if entries.len() > 1 {
             return Err(CacheError::DatabaseError(
                 "More than one entry found".to_string(),
             ));
         }
 
-        let entry = match entries.pop() {
-            Some(passwd) => passwd,
-            None => return Err(CacheError::NoRecord),
-        };
-
-        Ok(entry)
+        match entries.pop() {
+            Some(entry) => Ok(entry),
+            None => Err(CacheError::NoRecord),
+        }
     }
 }
