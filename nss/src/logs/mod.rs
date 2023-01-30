bbf5805857ee39a039792ac9223022da24ecb387
denisonbarbosa <denison.barbosa@canonical.com>
2023-01-30 11:39:27 -0400

    Improving the logs module
    - Integrated the log prefix inside the macros to avoid having to import
      the const in other modules.
    
    - Changed the initialization logic for the log to no longer run each
      time the nss library is called.
    
    - Added an option to log to stderr when building the crate for tests and
      when the env variable NSS_AAD_DEBUG is set to stderr

--- nss/src/logs/mod.rs
+++ nss/src/logs/mod.rs
@@ -1,28 +1,49 @@
-use std::env;
-
-use ctor::ctor;
+use env_logger::{Builder, Target};
+use log::{LevelFilter, Metadata};
+use std::{env, io::Write};
 use syslog::{BasicLogger, Facility, Formatter3164};
-
-pub const LOGPREFIX: &str = "nss_aad:";
 
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        log::debug!("{} {}", LOGPREFIX, format_args!($($arg)*));
+        let log_prefix = "nss_aad:";
+        log::debug!("{} {}", log_prefix, format_args!($($arg)*));
     }
 }
 
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        log::error!("{} {}", LOGPREFIX, format_args!($($arg)*));
+        let log_prefix = "nss_aad:";
+        log::error!("{} {}", log_prefix, format_args!($($arg)*));
     }
 }
 
-// init_logger initialize the logger with a default level set to info
-// log level can be set to debug by setting the environment variable NSS_AAD_DEBUG
-#[ctor]
-fn init_logger() {
+/// init_logger initialize the global logger with a default level set to info. This function is only
+/// required to be called once and is a no-op on subsequent calls.
+///
+/// The log level can be set to debug by setting the environment variable NSS_AAD_DEBUG.
+pub fn init_logger() {
+    if log::logger().enabled(&Metadata::builder().build()) {
+        return;
+    }
+
+    let mut level = LevelFilter::Info;
+    if let Ok(target) = env::var("NSS_AAD_DEBUG") {
+        level = LevelFilter::Debug;
+        match target {
+            s if s == *"stderr" => init_stderr_logger(level),
+            _ => init_sys_logger(level),
+        }
+    } else {
+        init_sys_logger(level);
+    }
+
+    debug!("Log level set to {:?}", level);
+}
+
+/// init_sys_logger initializes a global log that prints messages to the system logs.
+fn init_sys_logger(log_level: LevelFilter) {
     let formatter = Formatter3164 {
         facility: Facility::LOG_USER,
         hostname: None,
@@ -38,16 +59,23 @@ fn init_logger() {
         Ok(l) => l,
     };
 
-    let mut level = log::LevelFilter::Info;
-    let show_debug = env::var("NSS_AAD_DEBUG").unwrap_or_default();
-    if !show_debug.is_empty() {
-        level = log::LevelFilter::Debug;
-    }
     if let Err(err) = log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-        .map(|()| log::set_max_level(level))
+        .map(|()| log::set_max_level(log_level))
     {
         eprintln!("cannot set log level: {err:?}");
+        return;
     };
 
-    crate::debug!("Log level set to {:?}", level);
+    debug!("Log output set to syslog");
+}
+
+/// init_stderr_logger initializes a global log that prints the messages to stderr.
+fn init_stderr_logger(log_level: LevelFilter) {
+    Builder::new()
+        .format(|buf, msg| writeln!(buf, "{}", msg.args()))
+        .filter(None, log_level)
+        .target(Target::Stderr)
+        .init();
+
+    debug!("Log output set to stderr");
 }
