04d4ecc9d87d9540d3704e98c321067036adc9a5
denisonbarbosa <denison.barbosa@canonical.com>
2023-02-01 10:32:18 -0400

    Updating lib.rs to override the cache options
    Now the library overrides the cache options and the nss lookup when
    being built with the integration-tests feature

--- nss/src/lib.rs
+++ nss/src/lib.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "integration-tests")]
+use cache::CacheDBBuilder;
+#[cfg(feature = "integration-tests")]
+use ctor::ctor;
+#[cfg(feature = "integration-tests")]
 use std::env;
 
 #[macro_use]
@@ -47,14 +52,51 @@ fn cache_result_to_nss_status<T>(r: Result<T, CacheError>) -> Response<T> {
 fn new_cache() -> Result<CacheDB, CacheError> {
     let mut c = CacheDB::new();
 
-    if cfg!(feature = "integration_tests") {
-        let cache_dir = env::var("NSS_AAD_CACHEDIR").unwrap_or_default();
-        if !cache_dir.is_empty() {
-            c.with_db_path(&cache_dir);
-        }
-    }
+    #[cfg(feature = "integration-tests")]
+    override_cache_options(&mut c);
 
     c.build()
+}
+
+/// override_cache_options parses the NSS_AAD env variables and overrides the cache default options
+/// according to the specified values.
+#[cfg(feature = "integration-tests")]
+fn override_cache_options(c: &mut CacheDBBuilder) {
+    debug!("Overriding cache options for tests");
+
+    if let Ok(cache_dir) = env::var("NSS_AAD_CACHEDIR") {
+        c.with_db_path(&cache_dir);
+    }
+
+    if let Ok(root_uid) = env::var("NSS_AAD_ROOT_UID") {
+        c.with_root_uid(root_uid.parse().unwrap());
+    }
+
+    if let Ok(root_gid) = env::var("NSS_AAD_ROOT_GID") {
+        c.with_root_gid(root_gid.parse().unwrap());
+    }
+
+    if let Ok(shadow_gid) = env::var("NSS_AAD_SHADOW_GID") {
+        c.with_shadow_gid(shadow_gid.parse().unwrap());
+    }
+
+    if let Ok(shadow_mode) = env::var("NSS_AAD_SHADOWMODE") {
+        c.with_shadow_mode(shadow_mode.parse().unwrap());
+    }
+}
+
+#[cfg(feature = "integration-tests")]
+#[ctor]
+/// register_local_aad_nss_service_for_tests executes the C API to override the NSS lookup.
+fn register_local_aad_nss_service_for_tests() {
+    #[link(name = "db_override")]
+    extern "C" {
+        fn db_override();
+    }
+
+    unsafe {
+        db_override();
+    }
 }
 
 #[cfg(test)]
