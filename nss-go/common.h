ed06f13e7ab6b98d7fc92223568564f90422cce0
Denison Barbosa <denisonbarbosa@users.noreply.github.com>
2023-02-08 05:40:08 -0400

    Rust NSS: Removing C NSS from the project (#105)
    * Moving integration tests to the Rust NSS directory
    
    * Removing C NSS from the project
    
    * Removing action responsible for building the C NSS.

--- nss-go/common.h
+++
@@ -1,6 +0,0 @@
-#include <nss.h>
-#include <glib.h>
-
-#define UNUSED(x) (void)(x)
-
-enum nss_status fetch_info(const char *db, const char *name, const uid_t uid, GPtrArray *all_entries, guint *all_entries_index, gchar **entry, int *errnop);
